3f166abdeac82e4ef1da368d683a619852d48b56
Marcel Schüring <marcelschuering@web.de>
2020-12-28 21:49:26 +0100

    Lvl 1: User and Password

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+
+const userSchema = {
+    username: String,
+    password: String
+}
+
+const User = mongoose.model("user",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        }
+    })
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username: username},function(err,foundItem){
+        if(!err){
+            if(foundItem.password === password){
+                res.render("secrets");
+            }
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+app.listen(3000,function(res, req){
+    console.log("Server started on port 3000");
+})
