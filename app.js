5331ffcc547d58d11558086fb6748bbd269b803c
Marcel Schüring <marcelschuering@web.de>
2020-12-28 21:52:20 +0100

    Lvl 2: Encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -9,12 +10,16 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     username: String,
     password: String
-}
+});
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt,{secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("user",userSchema);
 
