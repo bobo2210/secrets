fe14374e05c5bc4eec6edcf86ab23719300c46e9
Marcel Schüring <marcelschuering@web.de>
2020-12-30 21:31:11 +0100

    Lvl 4: Bcrypt (Hash + Salt)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -18,6 +18,8 @@ const userSchema = new mongoose.Schema ({
     username: String,
     password: String
 });
+
+const saltRounds = 10;
 
 const User = mongoose.model("user",userSchema);
 
@@ -34,25 +36,32 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        username: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser = new User({
+            username: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }
+        })
     })
 });
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({username: username},function(err,foundItem){
         if(!err){
-            if(foundItem.password === password){
-                res.render("secrets");
+            if(foundItem){
+                bcrypt.compare(password,foundItem.password,function(err,result){
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                })
+
             }
         } else {
             console.log(err);
